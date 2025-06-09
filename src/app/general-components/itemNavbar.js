c40fff96f279746199bfd5c971bef93aba2fd8bb
EvaTP <evathpr@icloud.com>
2025-06-09 14:05:50 +0200

    add : link to profil page in navbar

--- src/app/general-components/itemNavbar.js
+++ src/app/general-components/itemNavbar.js
@@ -6,7 +6,7 @@ import layoutStyles from "../styles/layout.module.css";
 export default function ItemNavbar() {
   return (
     <div className="app_container">
-      <header className={layoutStyles.header}> 
+      <header className={layoutStyles.header}>
         <div className={layoutStyles.header_content}>
           <p className={layoutStyles.header_title}>
             <Image
@@ -16,57 +16,60 @@ export default function ItemNavbar() {
               height={30}
               priority
             />
-            Adaction</p>
-        </div>  
-        <p className={layoutStyles.header_subtitle}>Agir pour un environnement plus propre</p>     
+            Adaction
+          </p>
+        </div>
+        <p className={layoutStyles.header_subtitle}>
+          Agir pour un environnement plus propre
+        </p>
       </header>
 
-	  <div>
-      <nav className={layoutStyles.navbar}>
-        <div className={layoutStyles.navbar_container}>
-          <Link href="/dashboard" className={layoutStyles.navbar_link}>
-            <Image
-              src="/sprout.svg"
-              alt="icon-leaf"
-              width={25}
-              height={25}
-              priority
+      <div>
+        <nav className={layoutStyles.navbar}>
+          <div className={layoutStyles.navbar_container}>
+            <Link href="/dashboard" className={layoutStyles.navbar_link}>
+              <Image
+                src="/sprout.svg"
+                alt="icon-leaf"
+                width={25}
+                height={25}
+                priority
               />
-            <span>Dashboard</span>
-          </Link>
-          <Link href="/#" className={layoutStyles.navbar_link}>
-          <Image
-              src="/package-plus.svg"
-              alt="icon-package"
-              width={25}
-              height={25}
-              priority
+              <span>Dashboard</span>
+            </Link>
+            <Link href="/#" className={layoutStyles.navbar_link}>
+              <Image
+                src="/package-plus.svg"
+                alt="icon-package"
+                width={25}
+                height={25}
+                priority
               />
-            <span>Collectes</span>
-          </Link>
-		  <Link href="/donate" className={layoutStyles.navbar_link}>
-            <Image
-              src="/heart.svg"
-              alt="icon-heart"
-              width={25}
-              height={25}
-              priority
+              <span>Collectes</span>
+            </Link>
+            <Link href="/donate" className={layoutStyles.navbar_link}>
+              <Image
+                src="/heart.svg"
+                alt="icon-heart"
+                width={25}
+                height={25}
+                priority
               />
-            <span>Dons</span>
-          </Link>
-		  <Link href="/#" className={layoutStyles.navbar_link}>
-            <Image
-              src="/user.svg"
-              alt="icon-user"
-              width={25}
-              height={25}
-              priority
+              <span>Dons</span>
+            </Link>
+            <Link href="/profil" className={layoutStyles.navbar_link}>
+              <Image
+                src="/user.svg"
+                alt="icon-user"
+                width={25}
+                height={25}
+                priority
               />
-            <span>Profil</span>
-          </Link>
-        </div>
-      </nav>
-	  </div>
-	 </div>
+              <span>Profil</span>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 }
